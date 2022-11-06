49d91f4ccc2348b6b34deda7f1953fcb170b164a
Aira Sakuranomiya <1596356870@qq.com>
2022-11-06 09:31:27 +0800

    修小bug、添加线条动画

--- sys/js/main.js
+++ sys/js/main.js
@@ -10,10 +10,15 @@ $(".page-cover").click(function () {
   $(".login-window").css({
     transition: "0.1s cubic-bezier(0.6, 0, 1, 0.8)",
   });
+  setTimeout(function () {
+    $(
+      ".login-button, .login-animation-add, .login-animation-add-scaler, .login-animation-avatar-mover, .login-animation-avatar, .burst, .anim-welcome, .anim-nickname, .stripe-anim, .stripe-anim-line"
+    ).removeClass("logining");
+  }, 400);
 });
 
 $(".login-button").click(function () {
   $(
-    ".login-button, .login-animation-add, .login-animation-add-scaler, .login-animation-avatar-mover, .login-animation-avatar, .burst, .anim-welcome, .anim-nickname"
+    ".login-button, .login-animation-add, .login-animation-add-scaler, .login-animation-avatar-mover, .login-animation-avatar, .burst, .anim-welcome, .anim-nickname, .stripe-anim, .stripe-anim-line"
   ).addClass("logining");
 });
